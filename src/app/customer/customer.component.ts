ff367009cf67e0770f190520019ca389c9762d28
shirishma1 <shirishmasingirikonda1@gmail.com>
2024-10-06 22:00:30 -0500

    added customer component(customer.component.ts, customer.component.html)

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'customer',
+    standalone: true,
+    templateUrl: './customer.component.html'
+})
+
+export class CustomerComponent{
+    
+}
